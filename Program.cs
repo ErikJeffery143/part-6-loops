14480e21222a2e6997ac8b0aea8ae3a678692cb3
Erik Jeffery <JeffErik143@GoTVDSB.ca>
2021-10-05 14:27:04 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part_6_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[25];
+            Random rnd = new Random();
+            int min;
+            int max;
+
+            min = Convert.ToInt32(Console.ReadLine());
+            max = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            { 
+
+                numbers[i] = rnd.Next(min, max);
+                Console.WriteLine(numbers[i]);
+
+            }
+
+
+            Console.WriteLine("Press ENTER to Close....");
+
+
+
+
+                Console.ReadLine();
+        }
+    }
+}
